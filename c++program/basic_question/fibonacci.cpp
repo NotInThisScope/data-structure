0b69aa41eda152cc79e5ab35f53d272a46ef3321
NotInThisScope <62013353+NotInThisScope@users.noreply.github.com>
2020-05-24 22:44:02 -0700

    Update fibonacci.cpp
    Added a prompt to input number.

--- c++program/basic_question/fibonacci.cpp
+++ c++program/basic_question/fibonacci.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+
+using namespace std;  
+
 int main()
 {
     int n;
+    cout << "Enter a Number: ";
     cin>>n;
+    
     int a=0,b=1,sum=a+b;
+    
     while(a<=n)
     {
         cout<<a;
